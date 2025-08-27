99566f6f26769d43691a4e6da97eba29285c059a
mariapv45 <mariapv55@gmail.com>
2025-08-27 19:38:56 +1000

    feature: - created nav bar - installed button and card components from shadcnui - updated theme - added image for navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+export default function NavBar() {
+  return (
+    <header className='border-border sticky top-0 z-50 w-full border-b bg-stone-900'>
+      <div className='container px-2 py-6'>
+        <div className='flex items-end'>
+          <img
+            src='/images/branding/beer-stein.webp'
+            alt='Beer Stein'
+            className='size-16 sm:size-20 md:size-24 lg:size-28'
+          />
+          <h1 className='font-logo -ml-2 text-xl text-amber-400 sm:-ml-4 sm:text-2xl md:text-3xl lg:text-4xl'>
+            <div className='pl-4'>The</div>
+            <div>Enchanted Tavern</div>
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+}
